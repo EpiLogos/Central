af96ce23731bd355ba5d57c435bf9a6f619d6246
Epii <frank.g.taylor97@gmail.com>
2026-08-14 00:14:31 +0100

    ticket 9: export machine inspection Port

--- ctrl/sdk/index.js
+++ ctrl/sdk/index.js
@@ -1,3 +1,3 @@
 export { CONNECTOR_API_VERSION, defineConnector, validateConnector, validateConnectorManifest } from "./connector.js";
 export { runWorkDiscoveryConformance } from "./conformance.js";
-export { WorkDiscovery } from "./ports/work-discovery.js";
+export { WorkDiscovery, MachineInspection } from "./ports/work-discovery.js";
