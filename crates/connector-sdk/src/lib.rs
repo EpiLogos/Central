9948cf9037ef5d264d03254cc0601c1898264172
Epii <frank.g.taylor97@gmail.com>
2026-08-14 15:04:50 +0100

    rebase(#14): export native SDK contracts

--- crates/connector-sdk/src/lib.rs
+++ crates/connector-sdk/src/lib.rs
@@ -4,10 +4,12 @@ mod port;
 
 pub use conformance::{
     run_configuration_manager_conformance, run_machine_inspector_conformance,
-    run_package_manager_conformance, run_service_manager_conformance,
-    run_work_discovery_conformance, ConfigurationManagerConformanceFixture, ConformanceFailure,
-    ConformanceReport, MachineInspectorConformanceFixture, PackageManagerConformanceFixture,
-    ServiceManagerConformanceFixture, WorkDiscoveryConformanceFixture,
+    run_native_open_conformance, run_native_reveal_conformance, run_package_manager_conformance,
+    run_service_manager_conformance, run_tag_store_conformance, run_work_discovery_conformance,
+    ConfigurationManagerConformanceFixture, ConformanceFailure, ConformanceReport,
+    MachineInspectorConformanceFixture, NativeTargetConformanceFixture,
+    PackageManagerConformanceFixture, ServiceManagerConformanceFixture,
+    TagStoreConformanceFixture, WorkDiscoveryConformanceFixture,
 };
 pub use connector::{
     validate_connector_manifest, CapabilityProbe, Connector, ConnectorContext, ConnectorDiagnostics,
@@ -16,12 +18,15 @@ pub use connector::{
 };
 pub use port::{
     ConfigurationManager, ConfigurationStateRequest, MachineInspectionInput, MachineInspectionOutput,
-    MachineInspector, ObservedConfiguration, ObservedPackage, ObservedService, PackageManager,
-    PackageStateRequest, PortContract, PortError, PortErrorCode, PortOperationContract,
-    ReconciliationSourceReference, ServiceManager, ServiceStateRequest, StateChangePreview,
-    StateChangeResult, WorkDiscovery, WorkDiscoveryInput, WorkDiscoveryOutput, WorkItem,
+    MachineInspector, NativeOpen, NativeOpenInput, NativeOpenOutput, NativeReveal,
+    NativeRevealInput, NativeRevealOutput, ObservedConfiguration, ObservedPackage, ObservedService,
+    PackageManager, PackageStateRequest, PortContract, PortError, PortErrorCode,
+    PortOperationContract, ReconciliationSourceReference, ServiceManager, ServiceStateRequest,
+    StateChangePreview, StateChangeResult, TagReadInput, TagReadOutput, TagReplaceInput,
+    TagReplaceOutput, TagStore, WorkDiscovery, WorkDiscoveryInput, WorkDiscoveryOutput, WorkItem,
     CONFIGURATION_MANAGER_OPERATIONS, CONFIGURATION_MANAGER_PORT, MACHINE_INSPECTOR_OPERATIONS,
-    MACHINE_INSPECTOR_PORT, PACKAGE_MANAGER_OPERATIONS, PACKAGE_MANAGER_PORT,
-    SERVICE_MANAGER_OPERATIONS, SERVICE_MANAGER_PORT, WORK_DISCOVERY_OPERATIONS,
-    WORK_DISCOVERY_PORT,
+    MACHINE_INSPECTOR_PORT, NATIVE_OPEN_OPERATIONS, NATIVE_OPEN_PORT, NATIVE_REVEAL_OPERATIONS,
+    NATIVE_REVEAL_PORT, PACKAGE_MANAGER_OPERATIONS, PACKAGE_MANAGER_PORT,
+    SERVICE_MANAGER_OPERATIONS, SERVICE_MANAGER_PORT, TAG_STORE_OPERATIONS, TAG_STORE_PORT,
+    WORK_DISCOVERY_OPERATIONS, WORK_DISCOVERY_PORT,
 };
