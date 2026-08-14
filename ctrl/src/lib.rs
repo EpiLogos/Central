d19aa9ad945eb582253b65293b4e5510e8570b44
Epii <frank.g.taylor97@gmail.com>
2026-08-14 15:08:46 +0100

    rebase(#14): export composed native and reconciliation surfaces

--- ctrl/src/lib.rs
+++ ctrl/src/lib.rs
@@ -10,7 +10,7 @@ pub use action::{
     create_core_action_registry, ActionDescriptor, ActionExecutionContext, ActionInputDefinition,
     ActionInputSelection, ActionRegistry, MutationClass,
 };
-pub use cli::{run_cli, run_cli_with_surface, CliEnvironment, CliExecution};
+pub use cli::{run_cli, run_cli_with_runtime, run_cli_with_surface, CliEnvironment, CliExecution};
 pub use control::{
     locate_control_root, search_control, ControlSearchMatch, ControlSearchResult, ControlSourceRoot,
     SourceClass, CONTROL_ROOTS,
@@ -31,18 +31,21 @@ pub use picker::{
 };
 pub use central_connector_sdk::{
     run_configuration_manager_conformance, run_machine_inspector_conformance,
-    run_package_manager_conformance, run_service_manager_conformance,
-    validate_connector_manifest, CapabilityProbe, ConfigurationManager,
-    ConfigurationManagerConformanceFixture, ConfigurationStateRequest, Connector,
-    ConnectorContext, ConnectorDiagnostics, ConnectorManifest, ConnectorPortDeclaration,
-    ConnectorRegistry, ConnectorSummary, MachineInspectionInput, MachineInspectionOutput,
-    MachineInspector, MachineInspectorConformanceFixture, ObservedConfiguration, ObservedPackage,
-    ObservedService, PackageManager, PackageManagerConformanceFixture, PackageStateRequest,
-    PortContract, PortError, PortErrorCode, ReconciliationSourceReference, ServiceManager,
-    ServiceManagerConformanceFixture, ServiceStateRequest, StateChangePreview, StateChangeResult,
-    WorkDiscovery, WorkDiscoveryInput, WorkDiscoveryOutput, WorkItem, CONFIGURATION_MANAGER_PORT,
-    CONNECTOR_API_VERSION, MACHINE_INSPECTOR_PORT, PACKAGE_MANAGER_PORT, SERVICE_MANAGER_PORT,
-    WORK_DISCOVERY_PORT,
+    run_native_open_conformance, run_native_reveal_conformance, run_package_manager_conformance,
+    run_service_manager_conformance, run_tag_store_conformance, validate_connector_manifest,
+    CapabilityProbe, ConfigurationManager, ConfigurationManagerConformanceFixture,
+    ConfigurationStateRequest, Connector, ConnectorContext, ConnectorDiagnostics, ConnectorManifest,
+    ConnectorPortDeclaration, ConnectorRegistry, ConnectorSummary, MachineInspectionInput,
+    MachineInspectionOutput, MachineInspector, MachineInspectorConformanceFixture, NativeOpen,
+    NativeOpenInput, NativeOpenOutput, NativeReveal, NativeRevealInput, NativeRevealOutput,
+    NativeTargetConformanceFixture, ObservedConfiguration, ObservedPackage, ObservedService,
+    PackageManager, PackageManagerConformanceFixture, PackageStateRequest, PortContract, PortError,
+    PortErrorCode, ReconciliationSourceReference, ServiceManager, ServiceManagerConformanceFixture,
+    ServiceStateRequest, StateChangePreview, StateChangeResult, TagReadInput, TagReadOutput,
+    TagReplaceInput, TagReplaceOutput, TagStore, TagStoreConformanceFixture, WorkDiscovery,
+    WorkDiscoveryInput, WorkDiscoveryOutput, WorkItem, CONFIGURATION_MANAGER_PORT,
+    CONNECTOR_API_VERSION, MACHINE_INSPECTOR_PORT, NATIVE_OPEN_PORT, NATIVE_REVEAL_PORT,
+    PACKAGE_MANAGER_PORT, SERVICE_MANAGER_PORT, TAG_STORE_PORT, WORK_DISCOVERY_PORT,
 };
 pub use central_reference_connectors::{
     create_default_connector_registry, FilesystemWorkConnector, InMemoryMachineConnector,
