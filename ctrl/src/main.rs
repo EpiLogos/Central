51a913b67095f419efc0b3f48e1587920b4aebba
Epii <frank.g.taylor97@gmail.com>
2026-08-14 01:15:06 +0100

    format Rust entrypoint

--- ctrl/src/main.rs
+++ ctrl/src/main.rs
@@ -1,8 +1,11 @@
+use central_ctrl::{ProcessContext, run};
 use std::process::ExitCode;
-use central_ctrl::{run, ProcessContext};
 
 fn main() -> ExitCode {
-    let output = run(std::env::args().skip(1).collect(), ProcessContext::from_process());
+    let output = run(
+        std::env::args().skip(1).collect(),
+        ProcessContext::from_process(),
+    );
     println!("{}", output.render());
     ExitCode::from(output.exit_code as u8)
 }
