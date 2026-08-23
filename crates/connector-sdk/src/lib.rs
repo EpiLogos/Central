4d7a496cd453d0cb429736d805c5e73717fd595b
Epii <frank.g.taylor97@gmail.com>
2026-08-23 21:45:47 +0200

    feat(oi97): publish Automation and UserNotification SDK seams

--- crates/connector-sdk/src/lib.rs
+++ crates/connector-sdk/src/lib.rs
@@ -1,9 +1,12 @@
+mod automation_conformance;
 mod conformance;
 mod connector;
+mod notification;
 mod port;
 mod scoped_conformance;
 mod synchronization_conformance;
 
+pub use automation_conformance::{run_automation_conformance, AutomationConformanceFixture};
 pub use conformance::{
     run_configuration_manager_conformance, run_machine_inspector_conformance,
     run_native_open_conformance, run_native_reveal_conformance, run_package_manager_conformance,
@@ -18,20 +21,27 @@ pub use connector::{
     ConnectorManifest, ConnectorPortDeclaration, ConnectorRegistry, ConnectorResolution,
     ConnectorSummary, IneligibleConnector, ManifestError, CONNECTOR_API_VERSION,
 };
+pub use notification::{
+    NotificationAuthorizationState, NotificationCapabilities, NotificationCapabilityRequest,
+    NotificationDelivery, NotificationDeliveryState, NotificationRequest, UserNotification,
+    USER_NOTIFICATION_OPERATIONS, USER_NOTIFICATION_PORT,
+};
 pub use port::{
-    ConfigurationManager, ConfigurationStateRequest, MachineInspectionInput, MachineInspectionOutput,
-    MachineInspector, NativeOpen, NativeOpenInput, NativeOpenOutput, NativeReveal,
-    NativeRevealInput, NativeRevealOutput, ObservedConfiguration, ObservedPackage, ObservedService,
-    PackageManager, PackageStateRequest, PortContract, PortError, PortErrorCode,
-    PortOperationContract, ReconciliationSourceReference, ServiceManager, ServiceStateRequest,
-    StateChangePreview, StateChangeResult, SynchronizationRequest, Synchronizer, TagReadInput,
-    TagReadOutput, TagReplaceInput, TagReplaceOutput, TagStore, WorkDiscovery, WorkDiscoveryInput,
-    WorkDiscoveryOutput, WorkItem, CONFIGURATION_MANAGER_OPERATIONS, CONFIGURATION_MANAGER_PORT,
-    MACHINE_INSPECTOR_OPERATIONS, MACHINE_INSPECTOR_PORT, NATIVE_OPEN_OPERATIONS, NATIVE_OPEN_PORT,
-    NATIVE_REVEAL_OPERATIONS, NATIVE_REVEAL_PORT, PACKAGE_MANAGER_OPERATIONS,
-    PACKAGE_MANAGER_PORT, SERVICE_MANAGER_OPERATIONS, SERVICE_MANAGER_PORT,
-    SYNCHRONIZER_OPERATIONS, SYNCHRONIZER_PORT, TAG_STORE_OPERATIONS, TAG_STORE_PORT,
-    WORK_DISCOVERY_OPERATIONS, WORK_DISCOVERY_PORT,
+    Automation, AutomationRunInput, AutomationRunOutput, ConfigurationManager,
+    ConfigurationStateRequest, MachineInspectionInput, MachineInspectionOutput, MachineInspector,
+    NativeOpen, NativeOpenInput, NativeOpenOutput, NativeReveal, NativeRevealInput,
+    NativeRevealOutput, ObservedConfiguration, ObservedPackage, ObservedService, PackageManager,
+    PackageStateRequest, PortContract, PortError, PortErrorCode, PortOperationContract,
+    ReconciliationSourceReference, ServiceManager, ServiceStateRequest, StateChangePreview,
+    StateChangeResult, SynchronizationRequest, Synchronizer, TagReadInput, TagReadOutput,
+    TagReplaceInput, TagReplaceOutput, TagStore, WorkDiscovery, WorkDiscoveryInput,
+    WorkDiscoveryOutput, WorkItem, AUTOMATION_OPERATIONS, AUTOMATION_PORT,
+    CONFIGURATION_MANAGER_OPERATIONS, CONFIGURATION_MANAGER_PORT, MACHINE_INSPECTOR_OPERATIONS,
+    MACHINE_INSPECTOR_PORT, NATIVE_OPEN_OPERATIONS, NATIVE_OPEN_PORT, NATIVE_REVEAL_OPERATIONS,
+    NATIVE_REVEAL_PORT, PACKAGE_MANAGER_OPERATIONS, PACKAGE_MANAGER_PORT,
+    SERVICE_MANAGER_OPERATIONS, SERVICE_MANAGER_PORT, SYNCHRONIZER_OPERATIONS,
+    SYNCHRONIZER_PORT, TAG_STORE_OPERATIONS, TAG_STORE_PORT, WORK_DISCOVERY_OPERATIONS,
+    WORK_DISCOVERY_PORT,
 };
 pub use scoped_conformance::{
     run_scoped_machine_inspector_conformance, ScopedMachineInspectorConformanceFixture,
